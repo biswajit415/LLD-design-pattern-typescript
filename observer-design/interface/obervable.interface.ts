3b0bb5a6b97cbc43629f5dca016502fbfecc57f9
Biswajit Chatterjee <bismax12345@gmail.com>
2024-10-09 00:12:05 +0530

    removed unexpected

--- observer-design/interface/obervable.interface.ts
+++
@@ -1,8 +0,0 @@
-interface StockObservable {
-  name: string;
-  add(observer: NotificationObserver): void;
-  remove(observer: NotificationObserver): void;
-  notify(): void;
-  setStockCount(count: number): void;
-  getStockCount(): number;
-}
